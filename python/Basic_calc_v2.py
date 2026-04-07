049e8bd3d319c701a4fc91d790538b770adde4e3
rosero2110-oss <piperosero2110@gmail.com>
2026-04-07 18:48:45 -0500

    update: modify basic_calc_v2

--- python/Basic_calc_v2.py
+++ python/Basic_calc_v2.py
@@ -2,7 +2,15 @@
 but you can initialize them.
 """
 num1 = 0
+print ("Number 1 type: ", type(num1))
 num2 = 0
+print ("Number 2 type: ", type(num2))
+
+num1 = 1.5
+print ("Number 1 type: ", type(num1))
+num2 = 3.25
+print ("Number 2 type: ", type(num2))
+
 #inputs
 print("plase, enter first number: ")
 num1 = int(input (num1))
